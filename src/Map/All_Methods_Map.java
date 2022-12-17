eccd2763d7c39614b70922c9d93d4fd29a308600
Itsmeswapnil <mystudy75@gmail.com>
2022-12-18 00:00:20 +0530

    All method of MAP interfacce added by study_master

---
+++ src/Map/All_Methods_Map.java
@@ -0,0 +1,31 @@
+package Map;
+
+public class All_Methods_Map {
+	
+	public void put_method()
+	{
+		System.out.println("Put method from map interface");
+	}
+	
+	/*Map Interface define the 12 Methods
+	-----------------------------------------------
+	1) put(Object key, Object Value)
+	2) putAll(Map map)
+	3) get(Object key)
+	4) remove(Object key)
+	5) containsKey(Object key)
+	6) containsValue(Object value)
+	7) size()
+	8) isEmpty()
+	9) clear()
+	10) keySet()
+			return type is Set interface
+			it will return only keys 
+	11) values()	
+			return type is Collection interface
+			it will return only values
+	12) entrySet	
+			return type is Set interface
+			it will return key-value */
+
+}
