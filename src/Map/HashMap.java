0d93486567f823c74b5e602f15bf2d5837a37aa8
vahidashikalgar <vahidashikalgar313@gmai.com>
2022-12-17 23:52:24 +0530

    Details by of hashmap added by vahida.

--- src/Map/HashMap.java
+++ src/Map/HashMap.java
@@ -2,8 +2,20 @@ package Map;
 
 public class HashMap {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	
+	{
 		System.out.println("Hash Map class");
+		
 	}
+	
 
+//	- underlaying data structure is HashTable
+//	- Heterogenouse Object are allowed for both key-value pair
+//	- Duplicate key are not allowed but duplicate values are allowed
+//	- insertion order is not maintained
+//	- null key allowed only once
+//	- null value are allowed multiple time	
+//	-default capacity is 16
+	
 }
